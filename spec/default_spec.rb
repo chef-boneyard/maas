3d34031cae5c6e40e3277eeed610254fe6568139
JJ Asghar <jj@getchef.com>
2015-01-30 16:53:20 -0600

    adding better testing options

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe 'maas::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:runner) { ChefSpec::ServerRunner.new(UBUNTU_OPTS) }
+  let(:node) { runner.node }
+
+  let(:chef_run) do
+    node.set['lsb']['codename'] = 'ubuntu'
+    runner.converge(described_recipe)
+  end
 
   it 'requires the apt cookbook' do
     expect(chef_run).to include_recipe('apt')
