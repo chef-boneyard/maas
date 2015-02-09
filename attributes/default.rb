11b9abe4a5389fb48adfd303d20d3e0ab28f917b
JJ Asghar <jj@getchef.com>
2015-02-09 12:13:10 -0600

    updated for automaticly creating superuser

---
+++ attributes/default.rb
@@ -0,0 +1,9 @@
+# Create the Superuser
+default['maas']['create_superuser'] = true
+
+# Superuser name
+default['maas']['superuser_name'] = "admin"
+# Superuser password
+default['maas']['superuser_password'] = "admin"
+# Superuser email
+default['maas']['superuser_email'] = "fakeemail@fake.com"
