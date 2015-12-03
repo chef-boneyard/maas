9b68c54316ae142f9e33998ac7591c4d94d9a246
JJ Asghar <jj@chef.io>
2015-12-03 14:32:40 -0600

    updated documentation

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name 'maas'
-maintainer 'JJ Asghar'
-maintainer_email 'jj@chef.io'
+maintainer 'Chef Partner Engineering'
+maintainer_email 'partnereng@chef.io'
 license 'Apache 2'
 description 'Installs/Configures maas'
 long_description 'Installs/Configures maas'
-version '2.0.0'
+version '2.0.1'
 
 supports 'ubuntu', '>= 14.04'
 
